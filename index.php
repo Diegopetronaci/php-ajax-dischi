874e3a5d2b755bc40af9063d4221ace7a17fdbe4
Diego <Petronacidiego@gmail.com>
2021-02-02 16:28:23 +0100

    commit iniziale

---
+++ index.php
@@ -0,0 +1,80 @@
+<!-- Prima Milestone:
+Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente 
+i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi. -->
+
+<?php 
+    $dischi = [
+        [
+           "img" => "img/logo.png",
+           "title" => "Titolo",
+           "description" => "Descrizione"
+        ],
+        [
+           "img" => __DIR__ . "/img/logo.png",
+           "title" => "Titolo",
+           "description" => "Descrizione"
+        ],
+        [
+           "img" => __DIR__ . "/img/logo.png",
+           "title" => "Titolo",
+           "description" => "Descrizione"
+        ],
+        [
+           "img" => __DIR__ . "/img/logo.png",
+           "title" => "Titolo",
+           "description" => "Descrizione"
+        ],
+        [
+           "img" => __DIR__ . "/img/logo.png",
+           "title" => "Titolo",
+           "description" => "Descrizione"
+        ],
+        [
+           "img" => __DIR__ . "/img/logo.png",
+           "title" => "Titolo",
+           "description" => "Descrizione"
+        ],
+        [
+           "img" => __DIR__ . "/img/logo.png",
+           "title" => "Titolo",
+           "description" => "Descrizione"
+        ],
+        [
+           "img" => __DIR__ . "/img/logo.png",
+           "title" => "Titolo",
+           "description" => "Descrizione"
+        ],
+        [
+           "img" => __DIR__ . "/img/logo.png",
+           "title" => "Titolo",
+           "description" => "Descrizione"
+        ],
+        [
+           "img" => __DIR__ . "/img/logo.png",
+           "title" => "Titolo",
+           "description" => "Descrizione"
+        ],    
+    ]
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="container d_flex">
+            <?php foreach ($dischi as $disco => $value) { ?>
+                <div class="contenitore">
+                    <img src='$value["title"]' alt="">
+                    <h1> <?php echo $value["title"]; ?> </h1>
+                    <?php for ($i = 0; $i < count($value["description"]) ; $i++) { ?>
+                        <p> <?php echo $value["description"]; ?> </p>
+                    <?php } ?> 
+                </div>
+            <?php } ?>
+        </div>
+    </body>
+</html>
